3e20ebab3d275c31c0577c35c2098e722fe2ae25
khayrulmithu <khayrulmithu648@gmail.com>
2024-09-25 13:35:15 +0600

    Add project files.

---
+++ BasicInventoryManagementSystem/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace BasicInventoryManagementSystem
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
