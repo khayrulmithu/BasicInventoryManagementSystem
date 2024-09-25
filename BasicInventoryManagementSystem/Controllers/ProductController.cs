273232e856a0429f825e2a73443228361247b10d
khayrulmithu <khayrulmithu648@gmail.com>
2024-09-25 22:11:31 +0600

    Now we can Update, View and Delete an item from Products.

--- BasicInventoryManagementSystem/Controllers/ProductController.cs
+++ BasicInventoryManagementSystem/Controllers/ProductController.cs
@@ -41,6 +41,69 @@ namespace BasicInventoryManagementSystem.Controllers
 
             return View();
         }
+
+        [HttpGet]
+        public ActionResult UpdateProduct(int id)
+        {
+            Product pr = db.Products.Where(x => x.id == id).SingleOrDefault();
+            return View(pr);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateProduct(int id, Product pro)
+        {
+            Product pr = db.Products.Where(x => x.id == id).SingleOrDefault();
+            pr.Product_name = pro.Product_name;
+            pr.Product_qnty = pro.Product_qnty;
+            db.SaveChanges();
+            return RedirectToAction("DisplayProduct");
+        }
+
+        [HttpGet]
+        public ActionResult ProductDetails(int id)
+        {
+            Product pr = db.Products.Where(x => x.id == id).SingleOrDefault();
+            return View(pr);
+
+        }
+
+
+        [HttpPost]
+        public ActionResult ProductDetails(int id, Product pro)
+        {
+            Product pr = db.Products.Where(x => x.id == id).SingleOrDefault();
+            pr.Product_name=pro.Product_name;
+            pr.Product_qnty=pro.Product_qnty;
+            db.SaveChanges();
+            return View(pr);
+
+        }
+
+        [HttpGet]
+        public ActionResult DeleteProduct(int id)
+        {
+            Product pro = db.Products.Where(x => x.id == id).SingleOrDefault();
+            return View(pro);
+
+        }
+
+
+        [HttpPost]
+        public ActionResult DeleteProduct(int id, Product pro)
+        {
+            db.Products.Remove( db.Products.Where(x => x.id == id).SingleOrDefault());
+            db.SaveChanges();
+            return RedirectToAction("DisplayProduct");
+
+        }
+
+
+
+
+
+
+
+
     }
 
 }
