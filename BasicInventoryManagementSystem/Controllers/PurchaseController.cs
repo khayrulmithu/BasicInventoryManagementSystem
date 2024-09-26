66967225c079990a6c22ab97eb4885a2283e567c
khayrulmithu <khayrulmithu648@gmail.com>
2024-09-26 20:37:26 +0600

    Now we can purchase product and view purchased history.

---
+++ BasicInventoryManagementSystem/Controllers/PurchaseController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BasicInventoryManagementSystem.Models;
+
+namespace BasicInventoryManagementSystem.Controllers
+{
+    public class PurchaseController : Controller
+    {
+        InventoryEntities db = new InventoryEntities();
+        // GET: Purchase
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult DisplayPurchase()
+        {
+            List<Purchase> list = db.Purchases.OrderByDescending(x=>x.id).ToList();
+            return View(list);
+        }
+
+        [HttpGet]
+        public ActionResult PurchaseProduct()
+        {
+            List<string> list = db.Products.Select(x=>x.Product_name).ToList();
+            ViewBag.ProductName = new SelectList(list);
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult PurchaseProduct(Purchase pur)
+        {
+            db.Purchases.Add(pur);
+            db.SaveChanges();
+            return RedirectToAction("DisplayPurchase");
+        }
+
+
+
+    }
+}
